b1c2de53def013e9a3393758c9cb258503c3b94e
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-02-28 06:20:05 +0700

    Update ar.sh

--- ar.sh
+++ ar.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-
-apt update && apt -y install zip curl gdebi-core sudo
-
-wget http://mirrors.kernel.org/ubuntu/pool/main/n/ncurses/libtinfo6_6.2-0ubuntu2_amd64.deb
-
-sudo dpkg -i libtinfo6_6.2-0ubuntu2_amd64.deb
-
-cat /proc/meminfo | grep HugePages
-
-sudo sysctl -w vm.nr_hugepages=1000
-sudo bash -c "echo vm.nr_hugepages=1000 >> /etc/sysctl.conf"
-
-cat /proc/meminfo | grep HugePages
-
-wget https://github.com/ArweaveTeam/arweave/releases/download/N.2.5.1.0/arweave-2.5.1.0.linux-x86_64.tar.gz && tar xf arweave-2.5.1.0.linux-x86_64.tar.gz
-
-./bin/randomx-benchmark --mine --init 2 --threads 2 --jit --largePages
-
-./bin/start mine mining_addr zqqisDqckU55hSFeH8O2zxHXX6SZSn8ZhWjTpqipO8o peer 188.166.200.45 peer 188.166.192.169 peer 163.47.11.64 peer 139.59.51.59 peer 138.197.232.192
+lsb_release -a
+apt update && apt -y install wget curl zip sudo
+git clone https://github.com/virdpool/miner
+cd miner
+./install_ubuntu_20.04.sh
+./activate_mainnet.sh
+cp ./run.sh ./my_run.sh
+./my_run.sh
